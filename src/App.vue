9ba600a2e75ce09ce88143d31964e7f54d350c4a
Val Saven <val.saven@gmail.com>
2017-10-06 10:46:44 +0300

    Fix job title

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header>
       <h1><a href="/" class="transparent-link" title="Val Saven. Full-Stack Developer">Val Saven</a></h1>
-      <h2>Full-Stack Developer</h2>
+      <h2>Software Developer</h2>
     </header>
     <router-view></router-view>
   </div>
