00d4a1ba0563d7a96efde40ef12b323f786ccd1e
Val Saven <val.saven@gmail.com>
2017-12-17 11:48:20 +0300

    Add icons to contacts

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,6 +1,7 @@
 <template>
 <ul class="contacts">
-  <li v-for="(item, i) in contacts" :key=i>
+  <li class="contact" v-for="(item, i) in contacts" :key=i>
+    <i :class="item.icon"></i>
     <a :href="item.href" target="_blank" :title="item.title">{{item.label}}</a>
   </li>
 </ul>
@@ -15,21 +16,25 @@
           href: 'mailto:val.saven@gmail.com?subject=Message%20from%20valsaven.com',
           title: 'Contact Val Saven',
           label: 'Email',
+          icon: 'icon-mail-squared',
         },
         {
           href: 'https://www.linkedin.com/in/valsaven',
           title: 'Val Saven\'s CV on LinkedIn',
           label: 'LinkedIn',
+          icon: 'icon-linkedin-squared',
         },
         {
           href: 'https://github.com/valsaven',
           title: 'Val Saven on Github',
           label: 'GitHub',
+          icon: 'icon-github-squared',
         },
         {
           href: 'https://twitter.com/valsaven',
           title: 'Val Saven on Twitter',
           label: 'Twitter',
+          icon: 'icon-twitter-squared',
         }],
       }
     },
@@ -37,4 +42,25 @@
 </script>
 
 <style scoped>
+@import url('../assets/style/contacts-icons.css');
+
+.contacts {
+  margin: 20px;
+  padding: 0;
+  list-style: none;
+}
+
+.contact {
+  display: flex;
+  align-items: center;
+}
+
+.contact > i {
+  font-size: 2rem;
+  color: #fff;
+}
+
+.contact > a {
+  font-size: 1.2rem;
+}
 </style>
