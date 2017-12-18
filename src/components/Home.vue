dec382bb1fef76bdfcf7a86b7657b248d9cee9b4
Val Saven <val.saven@gmail.com>
2017-12-18 23:05:20 +0300

    Add home block

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,17 @@
 <template>
+  <div class="home">
+    <div class="home-item">
+      <h1>Site overview</h1>
+      <p>This website is a homepage of Val Saven.</p>
+      <p>If you want to contact me, please use the ones listed in the contacts menu.</p>
+    </div>
+    <div class="home-item">
+      <h1>Simple self-introduction</h1>
+      <p>My name is Val Saven. This is my working alias. My real name is Valery Savchenko.</p>
+      <p>I'm a software developer.</p>
+      <p>My favorite language is JavaScript, I develop on Windows or Linux.</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +24,17 @@
 </script>
 
 <style scoped>
+  .home {
+    background-color: var(--body-bg-color);
+  }
+
+  .home p {
+    color: var(--main-text-color);
+    margin: 10px 20px;
+  }
+
+  .home-item {
+    border: 1px solid var(--head-bg-color);
+    margin: 20px;
+  }
 </style>
