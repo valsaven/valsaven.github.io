8c69a92664047f62be23126e6067f9dcbc1e7cd9
Val Saven <val.saven@gmail.com>
2018-02-17 23:17:16 +0300

    Fix Books list

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card dark>
-    <v-card-title>
+    <v-card-title class="search">
       <v-text-field
         append-icon="search"
         label="Search"
@@ -9,70 +9,72 @@
         v-model="search"
       ></v-text-field>
     </v-card-title>
-    <v-data-iterator
-        content-tag="v-layout"
-        row wrap
-        :items="books"
-        :search="search"
-        :rows-per-page-items="rowsPerPageItems"
-        :pagination.sync="pagination"
-      >
+    <v-container class="books-list" fluid grid-list-md>
+      <v-data-iterator
+          content-tag="v-layout"
+          row wrap
+          :items="books"
+          :search="search"
+          :rows-per-page-items="rowsPerPageItems"
+          :pagination.sync="pagination"
+        >
 
-       <v-flex slot="item" slot-scope="props" xs12 sm6 md4 lg3>
-        <v-card>
-          <v-card-title>
-            <h4>{{ props.item.title }}</h4>
-          </v-card-title>
+        <v-flex slot="item" slot-scope="props" xs12 sm6 md4 lg3>
+          <v-card>
+            <v-card-title>
+              <h4>{{ props.item.title }}</h4>
+            </v-card-title>
 
-          <v-divider></v-divider>
+            <v-divider></v-divider>
 
-          <v-list dense>
-            <v-list-tile>
-              <v-list-tile-content>Author:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.author }}</v-list-tile-content>
-            </v-list-tile>
+            <v-list dense>
+              <v-list-tile>
+                <v-list-tile-content>Author:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.author }}</v-list-tile-content>
+              </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-content>Year:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.year }}</v-list-tile-content>
-            </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-content>Year:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.year }}</v-list-tile-content>
+              </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-content>Date read:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.dateRead }}</v-list-tile-content>
-            </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-content>Date read:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.dateRead }}</v-list-tile-content>
+              </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-content>Rating:</v-list-tile-content>
-              <v-list-tile-content class="align-end">
-                <div class="rating">
-                  <i class="material-icons star">star_rate</i>{{ props.item.rating }}
-                </div>
-              </v-list-tile-content>
-            </v-list-tile>
+              <v-list-tile>
+                <v-list-tile-content>Rating:</v-list-tile-content>
+                <v-list-tile-content class="align-end">
+                  <div class="rating">
+                    <i class="material-icons star">star_rate</i>{{ props.item.rating }}
+                  </div>
+                </v-list-tile-content>
+              </v-list-tile>
 
-            <v-list-tile>
-              <v-list-tile-content>Comment:</v-list-tile-content>
-              <v-list-tile-content class="align-end">{{ props.item.comment }}</v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-card>
-      </v-flex>
+              <v-list-tile>
+                <v-list-tile-content>Comment:</v-list-tile-content>
+                <v-list-tile-content class="align-end">{{ props.item.comment }}</v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-card>
+        </v-flex>
 
-      <template slot="items" slot-scope="props">
-        <td class="text-xs-center">{{ props.item.title }}</td>
-        <td class="text-xs-center">{{ props.item.author }}</td>
-        <td class="text-xs-center">{{ props.item.year }}</td>
-        <td class="text-xs-center">{{ props.item.dateRead }}</td>
-        <td class="text-xs-center">
-          <v-container class="rating">
-            <i class="material-icons star">star_rate</i>
-            {{ props.item.rating }}
-          </v-container>
-        </td>
-        <td class="text-xs-right">{{ props.item.comment }}</td>
-      </template>
-    </v-data-iterator>
+        <template slot="items" slot-scope="props">
+          <td class="text-xs-center">{{ props.item.title }}</td>
+          <td class="text-xs-center">{{ props.item.author }}</td>
+          <td class="text-xs-center">{{ props.item.year }}</td>
+          <td class="text-xs-center">{{ props.item.dateRead }}</td>
+          <td class="text-xs-center">
+            <v-container class="rating">
+              <i class="material-icons star">star_rate</i>
+              {{ props.item.rating }}
+            </v-container>
+          </td>
+          <td class="text-xs-right">{{ props.item.comment }}</td>
+        </template>
+      </v-data-iterator>
+    </v-container>
   </v-card>
 </template>
 
@@ -95,6 +97,8 @@ export default {
 </script>
 
 <style scoped>
+.search,
+.books-list,
 .table {
   background-color: var(--body-bg-color);
 }
