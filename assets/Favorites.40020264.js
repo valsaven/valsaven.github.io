68366e3160766b3d0211d7c91c7ac5607a67782a
Val Saven <val.saven@gmail.com>
2022-10-12 17:39:38 +0300

    v7.5.0 - Rewrite pages and components in Vue 3 TS style

---
+++ assets/Favorites.40020264.js
@@ -0,0 +1,1 @@
+import{_ as o,o as e,c as s,a as c}from"./index.2241c1cd.js";const t={},n={class:"block-body"},_=c("h2",null,"Coming soon...",-1),a=[_];function r(d,i){return e(),s("div",n,a)}const f=o(t,[["render",r]]);export{f as default};
