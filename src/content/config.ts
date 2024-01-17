0304b659b0a50bb1a773922d0b4450d753530b2d
Val Saven <val.saven@gmail.com>
2024-01-17 20:16:46 +0300

    v8.0.0
    - Refactor styles: Move from Tailwind to standard CSS
    
    Move to Astro
    - Add pictures for blog posts
    - Upgrade Astro 3.6 to Astro 4
    - Update favicon
    - Update styles
    - Update packages

---
+++ src/content/config.ts
@@ -0,0 +1,16 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		lang: z.string().optional(),
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog };
