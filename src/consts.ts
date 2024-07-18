d0602a5e9c3b67d0dc4b37db819e9ef06c4400de
Val Saven <val.saven@gmail.com>
2024-07-18 08:59:41 +0300

    v8.4.2
    - Update packages
    - Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'valsaven.com';
-export const SITE_DESCRIPTION = 'Val Saven. Senior Front End Engineer';
+export const SITE_DESCRIPTION = 'Val Saven. Senior Frontend Software Engineer';
